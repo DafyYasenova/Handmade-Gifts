042b73fc4889b64ef9e41e6883eefc7df15798f1
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-03-18 12:38:14 +0200

    add error component

--- client/src/app/pages/pages.module.ts
+++ client/src/app/pages/pages.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ErrorComponent } from './error/error.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    ErrorComponent
   ],
   imports: [
     CommonModule
@@ -16,6 +18,7 @@ import { AboutComponent } from './about/about.component';
   exports: [
     HomeComponent,
     AboutComponent,
+    ErrorComponent,
   ]
 })
 export class PagesModule { }
