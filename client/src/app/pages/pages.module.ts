4bdcea92b59d34e90a1d977d81637c028b3e6605
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-03-18 11:22:34 +0200

    add about component

--- client/src/app/pages/pages.module.ts
+++ client/src/app/pages/pages.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HomeComponent,
+    AboutComponent,
   ]
 })
 export class PagesModule { }
