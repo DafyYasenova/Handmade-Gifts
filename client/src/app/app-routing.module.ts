9f25128fd5eedfca33d76816f26db8039fa3b9b4
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-03-19 10:37:37 +0200

    add router module and routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch:'full', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  
+  {path:'login', component: LoginComponent},
+  {path:'register', component: RegisterComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
