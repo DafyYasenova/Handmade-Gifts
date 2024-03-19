5ad3b08524c0b2c3042f4f7b5006ef828ec7dc84
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-03-19 10:49:37 +0200

    add routing and footer logo scroll to top home page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,13 +4,23 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { ProfileComponent } from './user/profile/profile.component';
 
 const routes: Routes = [
   {path: '', pathMatch:'full', component: HomeComponent},
   {path: 'about', component: AboutComponent},
-  
+
+
+  // {path: 'create', component: CreateComponent},  
+  // {path: 'create', component: CreateComponent},  
+
   {path:'login', component: LoginComponent},
   {path:'register', component: RegisterComponent},
+  {path:'profile', component: ProfileComponent},
+
+  { path: '**', redirectTo: '/404' },
+  { path: '404', component: ErrorComponent},
 ];
 
 @NgModule({
--- client/src/app/core/footer/footer.component.ts
+++ client/src/app/core/footer/footer.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
+
+  constructor() { }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
 }
