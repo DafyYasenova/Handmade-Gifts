d09f1f3d576d5471c061f3383b6d232adacb519a
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-03-19 15:57:31 +0200

    update details component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -7,13 +7,15 @@ import { RegisterComponent } from './user/register/register.component';
 import { ErrorComponent } from './pages/error/error.component';
 import { ProfileComponent } from './user/profile/profile.component';
 import { GalleryComponent } from './products/gallery/gallery.component';
+import { DetailsComponent } from './products/details/details.component';
 
 const routes: Routes = [
   {path: '', pathMatch:'full', component: HomeComponent},
   {path: 'about', component: AboutComponent},
 
 
-  {path: 'gallery', component: GalleryComponent},  
+  {path: 'products', component: GalleryComponent},  
+  {path: 'products/:id', component: DetailsComponent},  
   // {path: 'create', component: CreateComponent},  
 
   {path:'login', component: LoginComponent},
