75a99e2eba9d5abbff9fa07e4544a466eea5b076
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-03-22 21:50:57 +0200

    add create

--- client/src/app/products/create/create.component.ts
+++ client/src/app/products/create/create.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { tap } from 'rxjs';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-create',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent {
+  constructor(private apiService: ApiService) { };
 
+  addProduct(event: Event, product: string, brand: string, imageUrl: string, description: string, price: number, category: string, status: string) {
+    event.preventDefault();
+    console.log({ product, brand, imageUrl, description, price, category, status });
+
+    this.apiService.createProduct(product, brand, imageUrl, description, price, category, status).subscribe((data)=> {
+    // this.apiService.createProduct(product, brand, imageUrl, description, price, category, status).pipe(tap((data) => {
+      console.log({ data })
+    })
+    // )
+  }
 }
