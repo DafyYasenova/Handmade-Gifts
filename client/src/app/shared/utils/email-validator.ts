cf094413c3fa89c991abfc50584bce3c2a28158d
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-03-23 22:16:07 +0200

    email validator

--- client/src/app/shared/utils/email-validator.ts
+++ client/src/app/shared/utils/email-validator.ts
@@ -7,10 +7,8 @@ export function emailValidator(domains: string[]): ValidatorFn {
     const regExp = new RegExp(`^[A-Za-z0-9]{4,}@(gmail|abv)\.(${domainString})$`);
 
     return (control) => {
-        const isEmailinvalid = control.value === '' ||regExp.test(control.value);
 
-        console.log('testing regex', isEmailinvalid)
-        console.log('control value:', control.value);
+        const isEmailinvalid = control.value === '' || regExp.test(control.value);
 
         return isEmailinvalid ? null : { emailValidator: true };
     }
--- client/src/app/shared/validators/email.directive.ts
+++ client/src/app/shared/validators/email.directive.ts
@@ -1,5 +1,6 @@
-import { Directive } from '@angular/core';
+import { Directive, Input } from '@angular/core';
 import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
+import { emailValidator } from '../utils/email-validator';
 
 @Directive({
   selector: '[appEmail]', 
@@ -13,9 +14,15 @@ import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@an
 })
 export class EmailDirective implements Validator{
 
+  @Input() appEmail: string[] = [];
+
   constructor() { }
   validate(control: AbstractControl<any, any>): ValidationErrors | null {
-    
+        // console.log('control', control.value);
+    const validatorFn = emailValidator(this.appEmail);
+
+    return validatorFn(control);
+    // console.log({ result })
   }
 
 }
