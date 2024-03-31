0303feb9d82b3553965fb5de9e66a3ae602b3b6d
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-03-31 17:01:23 +0300

    add delete modal functionality in details component

--- client/src/app/products/details/details.component.ts
+++ client/src/app/products/details/details.component.ts
@@ -16,6 +16,8 @@ export class DetailsComponent implements OnInit {
   productId: string = '';
   userId = localStorage.getItem('userId');
   productOwnerId: string = '';
+  showDeleteConfirmationModal: boolean = false;
+
   constructor(private apiService: ApiService, private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
@@ -27,12 +29,49 @@ export class DetailsComponent implements OnInit {
       this.product = product;
       this.productId = product._id as unknown as string;
       this.productOwnerId = product._ownerId as unknown as string;
-
-      
     })
   }
     toggleLike():void {
       this.liked = !this.liked;
+    }
+
+    showDeleteModal(): void {
+      this.showDeleteConfirmationModal = true;
+    }    
+
+    deleteProduct() {
+      // this.productId = this.route.snapshot.params['id'];
+  
+      console.log('delete')
+      // this.apiService.getOneProduct(this.productId).subscribe((product) => {
+      //   this.product = product;
+  
+        this.apiService.deleteProduct(this.product, this.productId).subscribe(
+          () => {
+  
+            console.log('Product deleted successfully');
+  
+            this.router.navigate(['/products']);
+          }, error => {
+  
+            console.error('Error deleting product:', error);
+  
+          });
+      }
+  
+      
+    
+    cancel() {
+      // this.productId = this.route.snapshot.params['id'];
+  
+      // this.apiService.getOneProduct(this.productId).subscribe((product) => {
+      //   this.product = product
+  
+      //   this.apiService.deleteProduct(this.product, this.productId);
+      //   this.router.navigate([`/products/${this.productId}`])
+      // })
+      this.showDeleteConfirmationModal = false;
+  
     }
   }
 
