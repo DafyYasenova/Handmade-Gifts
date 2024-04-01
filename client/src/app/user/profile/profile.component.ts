df191f7493a03065cd7869ffeb647e90127b5c90
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-04-01 20:01:55 +0300

    update profile component

--- client/src/app/user/profile/profile.component.ts
+++ client/src/app/user/profile/profile.component.ts
@@ -11,27 +11,30 @@ import { UserService } from '../user.service';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit{
+export class ProfileComponent implements OnInit {
+
   http: any;
   products: Product[] | null = null;
- userId = localStorage.getItem('userId');
- username = localStorage.getItem('username');
- email = localStorage.getItem('email');
- isLoading: boolean = true;
- 
- constructor(private apiService: ApiService,private userService: UserService ){}
 
- get isLoggedIn(): boolean {
-  return this.userService.isLogged;
-}
- ngOnInit(): void {
-  if (this.userId) {
-    this.apiService.getMyProducts(this.userId).subscribe((products) => {
-    console.log(products)
-    this.products = products;
-    });
-    this.isLoading = false;
+  userId = localStorage.getItem('userId');
+  username = localStorage.getItem('username');
+  email = localStorage.getItem('email');
+  isLoading: boolean = true;
+
+  constructor(private apiService: ApiService, private userService: UserService) { }
+
+  get isLoggedIn(): boolean {
+    return this.userService.isLogged;
   }
-}
- 
+  
+  ngOnInit(): void {
+    if (this.userId) {
+      this.apiService.getMyProducts(this.userId).subscribe((products) => {
+        console.log(products)
+        this.products = products;
+      });
+      this.isLoading = false;
+    }
+  }
+
 }
--- client/src/app/user/user.module.ts
+++ client/src/app/user/user.module.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -17,7 +18,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     RegisterComponent,
   ],
   imports: [
-    CommonModule, RouterModule, ReactiveFormsModule,
+    CommonModule, RouterModule, ReactiveFormsModule, SharedModule
   ],
   exports: [
     ProfileComponent,
