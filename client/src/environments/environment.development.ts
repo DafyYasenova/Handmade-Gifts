a7ef7aef6445f0042fb198ee734e550e94ba8ee3
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-04-28 18:04:01 +0300

    update client

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
     apiUrl: 'http://localhost:3030',
+    // apiUrl: 'https://api-h4cu6dgqfq-uc.a.run.app',
+    // apiUrl: 'http://localhost:5000/handmade-gifts-a8b18/us-central1/api'
   
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
 
     apiUrl: 'http://localhost:3030',
+    // apiUrl: 'https://api-h4cu6dgqfq-uc.a.run.app'
 }
